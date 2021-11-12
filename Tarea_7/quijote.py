9464a8d4acfd4713dadb7575ec5a734f0efe3c8e
Wargreimon501 <imperialdramon_01@hotmail.com>
2021-11-12 03:20:15 -0600

    Tarea

---
+++ Tarea_7/quijote.py
@@ -0,0 +1,25 @@
+def string_match_brute(A , P):
+  for i in range(len(A)-len(P) + 1 ):
+    for j in range(len(P)):
+      if P[j] == A[i+j]:
+         pass
+      else:
+        break
+    if j+1 == len(P) and A[i+j] == P[j]:
+      print(f"se encontro en la pocicion: {i}")   
+arch=open('el_quijote_ii.txt', encoding="utf8")
+f=arch.read() 
+p=int(input("que deceas buscar hidalfo preciona ;1 ,molino preciona; 2,merced preciona 3  ;"))
+if p==1:
+    patron="hidalfo"
+    entrada=f
+    S=string_match_brute(entrada,patron)
+if p==2:
+    patron="molino"
+    entrada=f
+    S=string_match_brute(entrada,patron)
+if p==3:
+    patron="merced"
+    entrada=f
+    S=string_match_brute(entrada,patron)
+
